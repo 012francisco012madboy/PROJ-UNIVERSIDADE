d96929c493f299987ba2dd01b3d59e736382c11d
Ariel Francisco <arielfrancisco690@gmail.com>
2025-07-15 01:54:28 +0100

    Adicionar arquivos de projeto.

---
+++ PROJ-UNIVERSIDADE/wwwroot/js/layout.js
@@ -0,0 +1,40 @@
+﻿const sidebar = document.querySelectorAll(".main-container .sidebar-content .side-each")
+
+sidebar.forEach(element => {
+    const title = element.querySelector(".side-each-title")
+
+    title.addEventListener('click', () => {
+        document.querySelectorAll('.side-each-child').forEach(each => {
+            each.classList.remove('active');
+        });
+
+        const child = title.nextElementSibling;
+
+        if (child && child.classList.contains('side-each-child')) {
+            child.classList.toggle('active');
+        }
+        else {
+            document.querySelectorAll('.side-each-title').forEach(each => {
+                each.classList.remove('active');
+            });
+
+            title.classList.add('active');
+        }
+    });
+
+    const child = element.querySelector(".side-each-child")
+
+    if (child) {
+        child.addEventListener('click', () => {
+            document.querySelectorAll('.side-each-child').forEach(each => {
+                each.classList.remove('active');
+            });
+
+            document.querySelectorAll('.side-each-title').forEach(each => {
+                each.classList.remove('active');
+            });
+
+            title.classList.add('active');
+        })
+    }
+});
